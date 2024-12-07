f66e60c5c339ab24ee6d7e61691b10f577eb42cc
ghifariaulia <ghifari.auliaz@gmail.com>
2024-12-07 14:37:18 +0700

    feat: add user detail view and endpoint, and get user ID endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import RegisterView, UserListView
+from .views import RegisterView, UserDetailView, UserListView, get_user_id
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('get_user_id/', get_user_id, name='get_user_id'),
     path('users/', UserListView.as_view(), name='users'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name='user'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from django.http import JsonResponse
 
 # Create your views here.
 class RegisterView(APIView):
@@ -27,3 +29,32 @@ class UserListView(APIView):
         users = CustomUser.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+    
+class UserDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, pk):
+        user = CustomUser.objects.get(id=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        user = CustomUser.objects.get(id=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        user = CustomUser.objects.get(id=pk)
+        user.delete()
+        return Response({'message': 'User deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([JWTAuthentication]) 
+def get_user_id(request):
+    user_id = request.user.id
+    return JsonResponse({'user_id': user_id})
